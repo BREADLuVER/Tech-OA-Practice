a669dd2a5067c40d6174bba450dc9de5e401b995
BREADLuVER <ry2050@nyu.edu>
2024-11-26 16:02:45 -0800

    Refactor optimizeIdentifiers function and move implementation to a new file

--- optimizeIdentifiers/resusion.py
+++ optimizeIdentifiers/resusion.py
@@ -1,11 +1,16 @@
 
 
 def optimizeIdentifiers(s: str) -> int:
+    if not s:
+        return 0
+    elif len(s) == 1:
+        return 0
+    
     target = s[0] + s[-1]
 
     def find(s, target, count):
         if len(s) < 1:
-            return -1
+            return 0
         temp = s[0]+s[-1]
         if temp == target:
             return count
@@ -14,7 +19,9 @@ def optimizeIdentifiers(s: str) -> int:
         remove_last = find(s[:-1], target, count + 1)
         return max(remove_first, remove_last)
         
-    return max(find(s[1:], target, 1), find(s[:-1], target, 1))
+    return find(s[1:], target, 1)
 
 print(optimizeIdentifiers("babdcaac"))
 print(optimizeIdentifiers("hchc"))
+print(optimizeIdentifiers("h"))
+print(optimizeIdentifiers(""))
